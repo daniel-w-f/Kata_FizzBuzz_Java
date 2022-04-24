9fc5394887cc55c9e622b3856a58c37dc42c8337
daniel-w-f <daniel_wilms@gmx.de>
2022-04-24 16:20:05 +0200

    Write numbers from 1 to 100

---
+++ 1/FizzBuzz.java
@@ -0,0 +1,11 @@
+/**
+ * FizzBuzz
+ */
+public class FizzBuzz {
+
+    public static void main(String[] args) {
+        for (int i = 1; i < 101; i++) {
+            System.out.println(i);
+        }
+    }
+}
