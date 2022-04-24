aff7c818e630ee4d37db8ae0057ffb4c86bd8f57
daniel-w-f <daniel_wilms@gmx.de>
2022-04-24 16:22:27 +0200

    Add FizzBuzz

--- 1/FizzBuzz.java
+++ 1/FizzBuzz.java
@@ -5,7 +5,9 @@ public class FizzBuzz {
 
     public static void main(String[] args) {
         for (int i = 1; i < 101; i++) {
-            if (i % 3 == 0) {
+            if (i % 15 == 0) {
+                System.out.println("FizzBuzz");
+            } else if (i % 3 == 0) {
                 System.out.println("Fizz");
             } else if (i % 5 == 0) {
                 System.out.println("Buzz");
