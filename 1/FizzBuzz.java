5be7da2276faa3befc86ddf9b211e59c8ef4f71c
daniel-w-f <daniel_wilms@gmx.de>
2022-04-24 16:21:36 +0200

    Add Buzz

--- 1/FizzBuzz.java
+++ 1/FizzBuzz.java
@@ -7,6 +7,8 @@ public class FizzBuzz {
         for (int i = 1; i < 101; i++) {
             if (i % 3 == 0) {
                 System.out.println("Fizz");
+            } else if (i % 5 == 0) {
+                System.out.println("Buzz");
             } else {
                 System.out.println(i);
             }
